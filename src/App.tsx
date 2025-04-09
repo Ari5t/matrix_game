803b5001078b3d34f35cefad3ed3fffc84edce2e
Yaroslav Burianov <yburyanov@gmail.com>
2025-04-09 22:44:23 +0300

    Update: added genaration matrix

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+
+const ROWS = 6
+const COLS = 6
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [matrix, setMatrix] = useState<number[][]>([[]])
+
+  useEffect(
+    function () {
+      if (matrix.length === ROWS && matrix[matrix.length - 1].length === COLS)
+        return
+
+      const timer = setInterval(() => {
+        setMatrix((prevMatrix) => {
+          const newMatrix = [...prevMatrix]
+          if (newMatrix[newMatrix.length - 1].length !== COLS) {
+            newMatrix[newMatrix.length - 1] = [
+              ...newMatrix[newMatrix.length - 1],
+              Math.random() > 0.5 ? 1 : 0,
+            ]
+          } else {
+            newMatrix[newMatrix.length] = [Math.random() > 0.5 ? 1 : 0]
+          }
+
+          return newMatrix
+        })
+      }, 500)
+
+      return function stopTimer() {
+        clearInterval(timer)
+      }
+    },
+    [matrix]
+  )
+
+  console.log('Matrix updated: ', matrix)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      {matrix.map((row, rowIndex) => (
+        <div key={rowIndex} style={{ display: 'flex' }}>
+          {row.map((cell, cellIndex) => (
+            <div
+              key={cellIndex}
+              style={{
+                width: '20px',
+                height: '20px',
+                backgroundColor: cell ? 'black' : 'white',
+                border: '1px solid gray',
+              }}
+            />
+          ))}
+        </div>
+      ))}
+    </div>
   )
 }
 
